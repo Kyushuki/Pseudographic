e5e51c0a3a26e881344efbdf7b806ace4f301f2d
Andrew <89681138+Kyushuki@users.noreply.github.com>
2023-03-23 21:07:06 +0300

    Вывод функции при помощи двумерного массива (#2)
    * Вывод функции при помощи двумерного массива
    
    * Вывод графика при помощи двумерного массива
    
    * График функции имеет координатную оси
    
    * Не криво поворачивается только линейная функция
    
    * Поворот! Оно вышло!
    
    * Вместе с выводом находит минимум % и максимум # по OY
    
    ---------
    
    Co-authored-by: Манойленко Андрей Валерьевич <addow1@yandex.ru>

--- PseudoGraphik_HM/PseudoGraphik_HM.cpp
+++ PseudoGraphik_HM/PseudoGraphik_HM.cpp
@@ -1,7 +1,7 @@
-﻿#include <iostream>
+#include <iostream>
 #include <math.h>
 
-using namespace std;
+using namespace std; //эта версия ветки не имеет каких-либо изменений, по сравнению с GraphArray потому что создал её поздно ночью и хочу спать
 
 double linear(double x) {
 	return x;
@@ -44,33 +44,59 @@ void drawSymbolPlot(double(&func)(double), double xMax, double xMin, int stepCou
 			minNormY = normalY[i];
 		}
 	}
-	double maxNormY = normalY[0];
+	int maxNormY = normalY[0];
 	for (int i = 0; i <= stepCount; i++) {  // находит максимальное значение у
 		if (normalY[i] > maxNormY) {
 			maxNormY = normalY[i];
 		}
 	}
-	for (int j = 0; j <= maxNormY - 1; j++) { // печатает ветку оси OY
-		cout << "_";
-	}
-	cout << "_ y" << endl;
-	for (int i = 0; i <= stepCount; i++) {    // выводит функцию
 
-		cout << "|";                          // вывод части рамки по оси х
-		for (int k = 0; k <= normalY[i] - 1; k++) {
-			cout << ' ';
-		}
-		if (normalY[i] == minNormY) { // проверка на минимальное значение по оси у
-			cout << "%" << endl;
-		}
-		else if (normalY[i] == maxNormY) { //проверка на максимальное значение по оси х
-			cout << "#" << endl;
-		}
-		else {
-			cout << '*' << endl;
-		}
+	string GraphA[stepCount + 2][maxNormY + 2];
 
+	for (int i = 0; i <= stepCount + 1; i++) { //заполняет массив пустыми значениями(пробелами)
+		for (int j = 0; j <= maxNormY + 1; j++) {
+			GraphA[i][j] = " ";
+		}
 	}
+	for (int j = 0; j <= maxNormY + 2; j++) { //заполняет ось ОУ в массиве
+		GraphA[0][j] = "|";
+	}
+
+	for (int i = 0; i <= stepCount + 1; i++) { //заполняет ось ОХ в массиве
+		GraphA[i][0] = "-";
+	}
+	for (int i = 0; i <= stepCount + 1; i++) { //заполняет массив значениями графика
+		for (int j = 0; j <= maxNormY + 1; j++) {
+			if (j == normalY[i]) {
+
+				if (normalY[i] == minNormY) { // проверка на минимальное значение по оси у
+					GraphA[i + 1][j + 1] = "%";
+				}
+				else if (normalY[i] == maxNormY) { //проверка на максимальное значение по оси х
+					GraphA[i + 1][j + 1] = "#";
+				}
+				else {
+					GraphA[i + 1][j + 1] = "*";
+				}
+			}
+		}
+	}
+	GraphA[0][maxNormY + 1] = "Y";
+	GraphA[stepCount + 1][0] = "X";
+
+	string copiedGraphA[maxNormY + 2][maxNormY + 2]; //поворачивает график функции
+	for (int i = 0; i <= maxNormY + 1; i++) {
+		for (int j = 0; j <= stepCount + 1; j++) {
+			copiedGraphA[i][j] = GraphA[j][maxNormY + 1 - i];
+		}
+	}
+	for (int i = 0; i <= maxNormY + 1; i++) { //рисует массив - график функции
+		for (int j = 0; j <= maxNormY + 1; j++) {
+			cout << copiedGraphA[i][j];
+		}
+		cout << endl;
+	}
+
 	cout << "x" << endl;
 	cout << " % - минимальное значение по Y, # - максимальное значение по Y" << endl;
 }
@@ -150,3 +176,5 @@ int main(int x)
 	}
 }
 
+
+}
